cdd74a373ff01ec2f883d298ed7e655d536aa093
fayment <fayment@github>
2020-01-21 18:49:10 +0800

    build

---
+++ precache-manifest.2107b18a177b1f3b23cd78c86ed4cc57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6ec1ec3678020a51a147fd850621ad8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "42c6508fa3aa5ff5d396",
+    "url": "/static/css/2.92a5c669.chunk.css"
+  },
+  {
+    "revision": "39cf06c0e291e94467e2",
+    "url": "/static/css/main.36996b6e.chunk.css"
+  },
+  {
+    "revision": "42c6508fa3aa5ff5d396",
+    "url": "/static/js/2.4020994b.chunk.js"
+  },
+  {
+    "revision": "39cf06c0e291e94467e2",
+    "url": "/static/js/main.fe613069.chunk.js"
+  },
+  {
+    "revision": "5baffe868f05ecb7d00a",
+    "url": "/static/js/runtime-main.6184c64a.js"
+  }
+]);
